923b2ae2b1894f582f0813b08999a6576a439b2e
nrusnac <nrusnac@tudelft.nl>
2024-04-16 14:07:45 +0200

    Added conflicts detected in between traces and algorithm outputs

--- pipeline/calculate_total.sh
+++ pipeline/calculate_total.sh
@@ -12,7 +12,8 @@ declare -A traces_stats
 declare -A algorithm_stats
 
 # Loop through each item in the traces directory
-printf "%-15s %-15s %-15s %-15s %-15s \n" "Program Name" "DR Trace" "DR Algo" "Trace Rate" "Algo Rate"
+output=$(printf "%-15s %-15s %-15s %-15s %-15s \n" "Program Name" "DR Trace" "DR Algo" "Trace Rate" "Algo Rate")
+output+=$'\n' 
 for item in "$traces_dir"/*; do
     if [ -d "$item" ]; then
         filecount=0
@@ -39,14 +40,19 @@ for item in "$traces_dir"/*; do
             data_race_in_algorithm=$(tail -n 1 "./results/$dirname/$filename_without_ext.out" | rev | cut -d' ' -f1 | rev)
             # echo $data_race_in_algorithm
             ((algorithm_stats["$dirname"]+=$data_race_in_algorithm>0))
-
+            if [ "$data_race_in_algorithm" -ne "$data_race_in_trace" ]; then
+                echo "Data race mistmatch in $dirname for execution $filename_without_ext"
+            fi
         done
         LC_NUMERIC="C"
         num_files=$(find ./traces/$dirname -maxdepth 1 -type f | wc -l)
         trace_avg=$(echo "${traces_stats[$dirname]}/$num_files*100" | bc -l)
         algorithm_avg=$(echo "${algorithm_stats[$dirname]}/$num_files*100" | bc -l)
-        printf "%-15s %-15d %-15d %-15.2f %-15.2f \n" $dirname "${traces_stats[$dirname]}" "${algorithm_stats[$dirname]}" $trace_avg $algorithm_avg
+        output+=$(printf "%-15s %-15d %-15d %-15.2f %-15.2f \n" $dirname "${traces_stats[$dirname]}" "${algorithm_stats[$dirname]}" $trace_avg $algorithm_avg)
+        output+=$'\n' 
     elif [ -f "$item" ]; then
         echo "Error: File found in traces directory"
     fi
 done
+printf "\n"
+echo "$output"
